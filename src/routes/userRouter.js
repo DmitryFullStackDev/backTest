65e036e391c7c6e7c4ffd76b55b20861b13cb898
dmitryReact <dmitry.dev.react@gmail.com>
2023-11-21 13:17:27 +0300

    fix

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,6 +3,6 @@ const router = express.Router()
 const userController = require('../controlles/userController')
 
 router.post('/createUser', userController.createUser)
-router.post('/extendSubscription', userController.extendSubscription)
+router.post('/updateUser', userController.extendSubscription)
 
 module.exports = router
